bdf5a2b022fe19007f73c2e37e0ae25cb379dcd9
André Sanches <sanchessls@gmail.com>
2023-09-20 09:44:35 +0100

    Separating screen configuration into an object

--- app/image_ai/read_image.py
+++ app/image_ai/read_image.py
@@ -11,10 +11,12 @@ cards_dir = os.path.join(os.getcwd(), 'app', 'image_ai', 'card_images_large_scre
 cards_images = []
 found_cards = []  # List to store matched cards
 found_cards_dealer = []  # List to store matched cards
-attempt_counter = 1
-last_message_is_not_found = False;
+attempt_counter = 1 ##counter of mismatch
+last_message_is_not_found = False
 
-# Initialize the attempt counter
+roi_configurations = ["LARGE SCREEN", "SMALL SCREEN", "ALTERNATIVE MONITOR"]
+
+
 
 
 for filename in os.listdir(cards_dir):
@@ -35,19 +37,22 @@ def print_no_cards_message():
         last_message_is_not_found = True;
 
 def search_card(screenshot, card , card_name):
-    # cv2.imshow("Captured Screenshot", screenshot)
-    # cv2.waitKey(0)  # Wait for a key press to close the window
+    #cv2.imshow("Captured Screenshot", screenshot)
+    #cv2.waitKey(0)  # Wait for a key press to close the window
 
     # Checking dealer cards
     res = cv2.matchTemplate(screenshot, card, cv2.TM_CCOEFF_NORMED)
     _, max_val, _, max_loc = cv2.minMaxLoc(res)
-    threshold = 0.97 # Adjust the threshold as needed
+    threshold = 0.97 # Adjust the threshold as needed    
     card_name_pos = {}
     if max_val >= threshold:
         # print(f"Found card '{card_name}' at pixel coordinates (x={max_loc[0]}, y={max_loc[1]})")
         card_name_pos["card"] = card_name
         card_name_pos["pos_x"] = max_loc[0]
         card_name_pos["pos_y"] = max_loc[1]
+    #else:        
+    #    if (max_val >= 0.65):
+    #        print(f"Treshhold: {max_val} card: {card_name}" )
         # loc = np.where(res >= threshold)
         # for pt in zip(*loc[::-1]):
         #     # Draw a rectangle around the matched area
@@ -69,6 +74,38 @@ def add_card_if_not_exists(found_cards, found_card, is_player):
         # if still not found is indeed a new card 
         if found_card not in found_cards:
             found_cards.append(found_card)
+            
+# Define the region of interest (ROI) coordinates
+def GetRoiDefinitions(configuration):
+    if configuration == "LARGE SCREEN": #currently
+        return {
+            "roi_x_player": 1750,
+            "roi_y_player": 900,
+            "roi_x_dealer": 1450,
+            "roi_y_dealer": 650,
+            "roi_width": 400,
+            "roi_height": 50
+        }
+    elif configuration == "SMALL SCREEN":
+        return {
+            "roi_x_player": 1250,
+            "roi_y_player": 650,
+            "roi_x_dealer": 1100,
+            "roi_y_dealer": 480,
+            "roi_width": 400,
+            "roi_height": 100
+        }
+    elif configuration == "ALTERNATIVE MONITOR": 
+        return {
+            "roi_x_player": 956,
+            "roi_y_player": 747,
+            "roi_x_dealer": 937,
+            "roi_y_dealer": 580,
+            "roi_width": 400,
+            "roi_height": 22
+        }
+    else:
+        raise ValueError("Invalid configuration type")
 
 if not cards_images:        
     print_no_cards_message()
@@ -76,43 +113,28 @@ if not cards_images:
 
 else:
 
-    if __name__ == '__main__':                  
+    if __name__ == '__main__':                   
         attempt_counter = 0
-        # # Define the region of interest (ROI) coordinates
-        # # SMALL SCREEN
-        # roi_x_player = 1250  # X-coordinate of the top-left corner of the ROI
-        # roi_y_player = 650  # Y-coordinate of the top-left corner of the ROI
-        # roi_width = 400  # Width of the ROI
-        # roi_height = 100  # Height of the ROI
-
-        # roi_x_dealer = 1100  # X-coordinate of the top-left corner of the ROI
-        # roi_y_dealer = 480  # Y-coordinate of the top-left corner of the ROI
-
-        # Define the region of interest (ROI) coordinates
-        # LARGE SCREEN
-        roi_x_player = 1750  # X-coordinate of the top-left corner of the ROI
-        roi_y_player = 900  # Y-coordinate of the top-left corner of the ROI
-        roi_width = 400  # Width of the ROI
-        roi_height = 50  # Height of the ROI
-
-        roi_x_dealer = 1450  # X-coordinate of the top-left corner of the ROI
-        roi_y_dealer = 650  # Y-coordinate of the top-left corner of the ROI
+        # Change this to select the desired configuration
+        chosen_configuration = roi_configurations[0]  
+        roi_definitions = GetRoiDefinitions(chosen_configuration)
 
         while True:
             # Capture the screen
-            # screenshot = pyautogui.screenshot()
-            screenshot = pyautogui.screenshot(region=(roi_x_player, roi_y_player, roi_width, roi_height))
+            # screenshot = pyautogui.screenshot()            
+
+            screenshot = pyautogui.screenshot(region=(roi_definitions["roi_x_player"], roi_definitions["roi_y_player"], roi_definitions["roi_width"], roi_definitions["roi_height"]))
             screenshot = np.array(screenshot)
             screenshot_gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
         
-            screenshot_dealer = pyautogui.screenshot(region=(roi_x_dealer, roi_y_dealer, roi_width, roi_height))
+            screenshot_dealer = pyautogui.screenshot(region=(roi_definitions["roi_x_dealer"], roi_definitions["roi_y_dealer"], roi_definitions["roi_width"], roi_definitions["roi_height"]))
             screenshot_dealer = np.array(screenshot_dealer)
             screenshot_dealer_gray = cv2.cvtColor(screenshot_dealer, cv2.COLOR_BGR2GRAY)
        
             # Iterate through each template and perform template matching
             for card_gray, card_name in cards_images:
-                # cv2.imshow("Card to match", card_gray)
-                # cv2.waitKey(0)
+                #cv2.imshow("Card to match", card_gray)
+                #cv2.waitKey(0)
             
                 # Checking player cards
                 found_card = search_card(screenshot_gray, card_gray , card_name)
@@ -148,7 +170,8 @@ else:
 
             # Display the captured screen with matches
             #cv2.imshow("Screen", screenshot)
-            #cv2.waitKey(0)  # Wait for a key press to close the window
+            #cv2.imshow("Screen2", screenshot_dealer)
+            #cv2.waitKey(0)  # Wait for a key press to close the window            
 
 
             # Exit when 'q' key is pressed
